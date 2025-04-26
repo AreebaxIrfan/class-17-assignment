6dac1899d00eda2aeedfe4533b9af140cc029e51
Areeba Irfan <161799302+AreebaxIrfan@users.noreply.github.com>
2025-04-26 16:02:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import Form from "./components/Form";
-
 function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <Form />
     </main>
   );
 }
-
 export default Home;
